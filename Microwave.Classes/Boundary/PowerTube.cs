423e5322a2f27eccd76a2795c4cc148f9fb7c50f
Mathias Schjødt-Bavngaard <au618037@uni.au.dk>
2022-11-17 11:34:24 +0100

    Added Max power in Powertube

--- Microwave.Classes/Boundary/PowerTube.cs
+++ Microwave.Classes/Boundary/PowerTube.cs
@@ -11,6 +11,8 @@ namespace Microwave.Classes.Boundary
 
         private bool IsOn = false;
 
+        private int MaxPowerInWatts { get; set; }
+
         public PowerTube(IOutput output)
         {
             myOutput = output;
@@ -18,9 +20,9 @@ namespace Microwave.Classes.Boundary
 
         public void TurnOn(int power)
         {
-            if (power < 1 || 700 < power)
+            if (power < 1 || MaxPowerInWatts < power)
             {
-                throw new ArgumentOutOfRangeException("power", power, "Must be between 1 and 700 (incl.)");
+                throw new ArgumentOutOfRangeException("power", power, $"Must be between 1 and {MaxPowerInWatts}  (incl.)");
             }
 
             if (IsOn)
@@ -41,5 +43,12 @@ namespace Microwave.Classes.Boundary
 
             IsOn = false;
         }
+
+        public int GetMaxPowerInWatts()
+        {
+            return MaxPowerInWatts;
+        }
+
+       
     }
 }
